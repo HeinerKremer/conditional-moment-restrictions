b9c990ca54dbe326077c18e92fb27ef4fbde8e22
HeinerKremer <heiner.kremer@tuebingen.mpg.de>
2022-09-20 14:26:19 +0200

    fixed cluster submission script

--- cluster/generate_submission_files.py
+++ cluster/generate_submission_files.py
@@ -37,12 +37,12 @@ max_parallel_rollouts = None
 def get_run_path():
     path = os.path.realpath(__file__)
     path, file = os.path.split(path)
-    while file != 'Functional-GEL' and path != '/':
+    while file != 'Kernel-EL' and path != '/':
         path, file = os.path.split(path)
-    return path + '/Functional-GEL'
+    return path + '/Kernel-EL'
 
 path = get_run_path()
-venvpath = path + '/fgel_venv'
+venvpath = path + '/kel_venv'
 
 # ----------------
 
