b339cc5a937cc488750d0059aa5af535c4a8a92e
Yassine <yasnemou@gmail.com>
2022-10-11 18:10:15 +0200

    Made heteroskedastic exp 3d and add cluster config file.

--- kel/default_config.py
+++ kel/default_config.py
@@ -165,7 +165,7 @@ methods = {
             'estimator_class': KernelELNeural,
             'estimator_kwargs': {
                 "batch_training": True,
-                "batch_size": 200,
+                "batch_size": 256,
                 "n_random_features": 10000,
                 "max_num_epochs": 20000,
                 "burn_in_cycles": 5,
